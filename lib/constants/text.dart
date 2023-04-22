a2dd5977473f873fdead4a172be082378810e43b
Vladimir Izmaylov <holybuddha@yandex.ru>
2023-04-22 21:55:33 +0300

    Add text to const for camera snackbar message

--- lib/constants/text.dart
+++ lib/constants/text.dart
@@ -1,4 +1,6 @@
 class TextConsts {
   static const textForAppBarTitle = 'Camera Test App';
   static const textForCameraButton = 'Press the button to take a shot';
+  static const textForCameraErrorTitle = 'Error';
+  static const textForCamereErrorMain = 'Image not selected';
 }
