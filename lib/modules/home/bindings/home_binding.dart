69f5a725ee5d1ed880b3c43dd966974123618c41
Vladimir Izmaylov <holybuddha@yandex.ru>
2023-04-22 21:54:00 +0300

    Add Home bindings

---
+++ lib/modules/home/bindings/home_binding.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+import 'package:camera_test_app/modules/home/controllers/home_controller.dart';
+
+class HomeBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<HomeController>(
+      () => HomeController(),
+    );
+  }
+}
