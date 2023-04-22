2eaadcbf9e7c28aa271f1fb9397c3722aa178a25
Vladimir Izmaylov <holybuddha@yandex.ru>
2023-04-22 21:53:12 +0300

    Add Home Page

--- lib/modules/home/views/home_page.dart
+++ lib/modules/home/views/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:camera_test_app/constants/text.dart';
+import 'package:camera_test_app/modules/home/controllers/home_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends GetView<HomeController> {
   const HomePage({super.key});
 
   @override
@@ -16,9 +19,13 @@ class HomePage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            const Text(TextConsts.textForCameraButton),
+            Obx(() => controller.selectedImagePath.value == ''
+                ? const Text(TextConsts.textForCameraButton)
+                : Text(controller.selectedImagePath.string, textAlign: TextAlign.center,)),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                controller.getImage(ImageSource.camera);
+              },
               child: const Icon(Icons.camera_alt, color: Colors.black),
               style: ElevatedButton.styleFrom(
                 shape: const CircleBorder(),
