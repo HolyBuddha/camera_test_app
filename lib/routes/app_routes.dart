718de159e176a8744cad29eb1b112c98b0b840a8
Vladimir Izmaylov <holybuddha@yandex.ru>
2023-04-22 21:54:13 +0300

    Add Routes

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,15 +1,16 @@
+import 'package:camera_test_app/modules/home/bindings/home_binding.dart';
 import 'package:camera_test_app/modules/home/views/home_page.dart';
 import 'package:get/get.dart';
 
 
 class AppRoutes {
   static const String homePage = '/homepage;';
-
 }
 
 final routes = [
   GetPage(
     name: AppRoutes.homePage,
     page: () => const HomePage(),
+    binding: HomeBinding()
   ),
 ];
