04ca5d601c45ac3d192ab70560f0432048252f36
Vladimir Izmaylov <holybuddha@yandex.ru>
2023-04-24 13:26:43 +0300

    add pages router

---
+++ lib/routes/app_pages.dart
@@ -0,0 +1,21 @@
+import 'package:camera_test_app/modules/home/binding/home_binding.dart';
+import 'package:camera_test_app/modules/home/view/home_page.dart';
+import 'package:camera_test_app/modules/image_details/bindings/image_details_binding.dart';
+import 'package:camera_test_app/modules/image_details/view/image_details_page.dart';
+import 'package:camera_test_app/routes/app_routes.dart';
+import 'package:get/get.dart';
+
+class AppPages {
+  static final List<GetPage> pages = [
+    GetPage(
+      name: AppRoutes.INITIAL,
+      page: () => const HomePage(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: AppRoutes.imageDetails,
+      page: () => const ImageDetailsPage(),
+      binding: ImageDetailsBinding(),
+    ),
+  ];
+}
